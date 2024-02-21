404d7f7b25a976c223081fd35cd2e5102d500461
alcherniaev <playtowin19@gmail.com>
2024-02-21 13:04:21 +0300

    shad bala continue

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ sklonenie = {
     5: 1388,
     6: 1440
 }
+kranty_mult = {
+    0: 362,
+    1: 341,
+    2: 299,
+    3: 236,
+    4: 150,
+    5: 52,
+
+}
 def ravnodenstvie_distance(dolgota, ayanamsha=[21,16] ):
     ayanamsha_minutes = ayanamsha[0]*60 + ayanamsha[1]
     dolgota_minutes = dolgota[0]*60 + dolgota[1]
@@ -32,13 +41,25 @@ def ravnodenstvie_distance(dolgota, ayanamsha=[21,16] ):
     print(f"bhudja_ostatok = {bhudja_ostatok}")
     print(f"bhudja_chastnoe = {bhudja_chastnoe}")
     
-
+    kranty = sklonenie[bhudja_ostatok] + ((bhudja_chastnoe[0]*60+bhudja_chastnoe[1]) / 15*60) * kranty_mult[bhudja_ostatok]
+    print(f"kranty = {kranty//60, kranty%60}")
 ravnodenstvie_distance([202, 10])
-ravnodenstvie_distance([332, 33])
+"""ravnodenstvie_distance([332, 33])
 ravnodenstvie_distance([250, 47])
 ravnodenstvie_distance([202, 48])
 ravnodenstvie_distance([105, 17])
 ravnodenstvie_distance([192, 26])
 ravnodenstvie_distance([145, 39])
+"""
+a = (8*60 + 37)
+b = 15*60 
+c = (a/b) * 236
+d = int(c + 1002)
 
+def ayana_bala(kranti):
+    res = ((24 - kranti) * 60) / 48
+    #res = res * 2 # сурья онли x2
+    print(f"ayana bala: {res}")
 
+res = 362 + (7*60 + 10) / 15*60 * 341
+print(res//60, res%60)
