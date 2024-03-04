83a0083062c59bb2d30df9d4cdc41835fde9dcc7
alcherniaev <playtowin19@gmail.com>
2024-03-04 14:44:57 +0300

    shad bala continue

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ kranty_mult = {
 def ravnodenstvie_distance(dolgota, ayanamsha=[21,16] ):
     ayanamsha_minutes = ayanamsha[0]*60 + ayanamsha[1]
     dolgota_minutes = dolgota[0]*60 + dolgota[1]
-    dolgota_ayanamsha_minutes = dolgota_minutes
+    dolgota_ayanamsha_minutes = dolgota_minutes + ayanamsha_minutes
     distance = 360*60 - dolgota_ayanamsha_minutes 
 
     if dolgota_minutes < 90*60: 
@@ -40,10 +40,13 @@ def ravnodenstvie_distance(dolgota, ayanamsha=[21,16] ):
     bhudja_chastnoe = [bhudja[0]%15, bhudja[1]]
     print(f"bhudja_ostatok = {bhudja_ostatok}")
     print(f"bhudja_chastnoe = {bhudja_chastnoe}")
-    
-    kranty = sklonenie[bhudja_ostatok] + ((bhudja_chastnoe[0]*60+bhudja_chastnoe[1]) / 15*60) * kranty_mult[bhudja_ostatok]
+    a = (bhudja_chastnoe[0]*60+bhudja_chastnoe[1])
+    b = 15*60
+    c = (a/b) * kranty_mult[bhudja_ostatok]
+    kranty = int(c + 362)
     print(f"kranty = {kranty//60, kranty%60}")
-ravnodenstvie_distance([202, 10])
+    return kranty/60
+kranty = ravnodenstvie_distance([180, 54])
 """ravnodenstvie_distance([332, 33])
 ravnodenstvie_distance([250, 47])
 ravnodenstvie_distance([202, 48])
@@ -51,15 +54,11 @@ ravnodenstvie_distance([105, 17])
 ravnodenstvie_distance([192, 26])
 ravnodenstvie_distance([145, 39])
 """
-a = (8*60 + 37)
-b = 15*60 
-c = (a/b) * 236
-d = int(c + 1002)
+
 
 def ayana_bala(kranti):
     res = ((24 - kranti) * 60) / 48
     #res = res * 2 # сурья онли x2
     print(f"ayana bala: {res}")
 
-res = 362 + (7*60 + 10) / 15*60 * 341
-print(res//60, res%60)
+ayana_bala(kranty)
